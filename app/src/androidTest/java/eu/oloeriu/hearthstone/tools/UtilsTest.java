9b140026c439aa7c52620b526a1056c8a7d3bcee
Bogdan Oloeriu <bogdan.oloeriu@gmail.com>
2018-03-15 23:34:32 +0100

    Started to work on load cards from json

---
+++ app/src/androidTest/java/eu/oloeriu/hearthstone/tools/UtilsTest.java
@@ -0,0 +1,17 @@
+package eu.oloeriu.hearthstone.tools;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Bogdan Oloeriu on 15/03/2018.
+ */
+public class UtilsTest {
+    @Test
+    public void loadCardsFromJson() throws Exception {
+        
+        assertEquals(4, 2 + 2);
+    }
+
+}
---
+++ app/src/main/java/eu/oloeriu/hearthstone/tools/Utils.java
@@ -0,0 +1,14 @@
+package eu.oloeriu.hearthstone.tools;
+
+import android.util.Log;
+
+/**
+ * Created by Bogdan Oloeriu on 15/03/2018.
+ */
+
+public class Utils {
+    private static String logTag = Constants.LOG_TAG;
+    public static void loadCardsFromJson(){
+        Log.d(logTag, "Initiate loadLocalCards()");
+    }
+}
