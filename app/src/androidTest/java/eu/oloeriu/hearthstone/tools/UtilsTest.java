5b4d92951dc605e51c82cf16f958411257079205
Bogdan Oloeriu <bogdan.oloeriu@gmail.com>
2018-03-16 01:06:09 +0100

    loaded file from raw folder

--- app/src/androidTest/java/eu/oloeriu/hearthstone/tools/UtilsTest.java
+++ app/src/androidTest/java/eu/oloeriu/hearthstone/tools/UtilsTest.java
@@ -1,17 +1,22 @@
 package eu.oloeriu.hearthstone.tools;
 
+import android.content.res.Resources;
+import android.support.test.InstrumentationRegistry;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 /**
- * Created by Bogdan Oloeriu on 15/03/2018.
+ * Created by Bogdan Oloeriu on 16/03/2018.
  */
 public class UtilsTest {
     @Test
     public void loadCardsFromJson() throws Exception {
-        
-        assertEquals(4, 2 + 2);
+        Resources resources = InstrumentationRegistry.getTargetContext().getResources();
+        Utils.loadCardsFromJson(resources);
+        //Resources resources = getInstru
+        //Utils.loadCardsFromJson()
     }
 
 }
--- app/src/main/java/eu/oloeriu/hearthstone/tools/Utils.java
+++ app/src/main/java/eu/oloeriu/hearthstone/tools/Utils.java
@@ -1,6 +1,14 @@
 package eu.oloeriu.hearthstone.tools;
 
+import android.content.res.Resources;
 import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import eu.oloeriu.hearthstone.R;
 
 /**
  * Created by Bogdan Oloeriu on 15/03/2018.
@@ -8,7 +16,23 @@ import android.util.Log;
 
 public class Utils {
     private static String logTag = Constants.LOG_TAG;
-    public static void loadCardsFromJson(){
+    public static List<Card> loadCardsFromJson(Resources resources){
+
+
+        String json = null;
+        try {
+            InputStream is = resources.openRawResource(R.raw.cards);
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+            Log.d(logTag,json);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
         Log.d(logTag, "Initiate loadLocalCards()");
+        return new ArrayList<>();
     }
 }
