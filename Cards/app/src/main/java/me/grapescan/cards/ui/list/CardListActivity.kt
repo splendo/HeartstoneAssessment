ebb2ba5277f35bdb699f33ebf364d81401cc3803
Dmitrii Soldatov <dmitrii.soldatov@doclerholding.com>
2019-05-27 09:47:29 +0200

    Removed toolbar, made status bar and navigation bar transparent.

--- Cards/app/src/main/java/me/grapescan/cards/ui/list/CardListActivity.kt
+++ Cards/app/src/main/java/me/grapescan/cards/ui/list/CardListActivity.kt
@@ -1,7 +1,10 @@
 package me.grapescan.cards.ui.list
 
+import android.os.Build
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -18,13 +21,22 @@ class CardListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_card_list)
-        viewModel.cards.observe(this@CardListActivity, Observer<List<Card>> { adapter.submitList(it) })
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        }
         findViewById<RecyclerView>(R.id.card_list).run {
+            ViewCompat.setOnApplyWindowInsetsListener(this) { view, insets ->
+                this.setPadding(paddingLeft, insets.systemWindowInsetTop, paddingRight, insets.systemWindowInsetBottom)
+                insets.replaceSystemWindowInsets(insets.systemWindowInsetLeft, 0, insets.systemWindowInsetRight, 0)
+            }
             this@run.adapter = this@CardListActivity.adapter.apply {
                 onItemClick = { startActivity(CardDetailsActivity.createIntent(this@CardListActivity, it.id)) }
             }
             layoutManager = GridLayoutManager(this@CardListActivity, 3)
         }
+        viewModel.cards.observe(this@CardListActivity, Observer<List<Card>> { adapter.submitList(it) })
     }
 }
 
