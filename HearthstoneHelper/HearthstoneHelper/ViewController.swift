41e14a1b95713f49667a1e01851f7ec9c7f4dc55
Maxim Berezhnoy <maxim.berezhnoy@gmail.com>
2019-12-12 21:15:25 +0100

    Initial project setup + .gitignore

---
+++ HearthstoneHelper/HearthstoneHelper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HearthstoneHelper
+//
+//  Created by maxim on 12/12/2019.
+//  Copyright © 2019 rencevio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
