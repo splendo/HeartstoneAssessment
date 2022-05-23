2792536bd53bf5e1fb2796f4aeac9f62d9e27ebb
Leonid Kapanen <leokapanen@gmail.com>
2022-05-23 14:38:34 +0300

    Fresco library initialization was added.

--- app/src/main/java/com/kapanen/hearthstoneassessment/App.kt
+++ app/src/main/java/com/kapanen/hearthstoneassessment/App.kt
@@ -1,6 +1,7 @@
 package com.kapanen.hearthstoneassessment
 
 import androidx.multidex.MultiDexApplication
+import com.facebook.drawee.backends.pipeline.Fresco
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 import timber.log.Timber.Forest.plant
@@ -11,12 +12,17 @@ class App : MultiDexApplication() {
     override fun onCreate() {
         super.onCreate()
         initTimber()
+        initFresco()
     }
 
     private fun initTimber() {
         if (BuildConfig.DEBUG) {
             plant(Timber.DebugTree())
         }
+    }
+
+    private fun initFresco() {
+        Fresco.initialize(this)
     }
 
 }
