18ea5c150956137c95c5f576976250462c1e84be
Leonid Kapanen <leokapanen@gmail.com>
2022-05-25 00:03:41 +0300

    Window action bar was removed.

--- app/src/main/java/com/kapanen/hearthstoneassessment/MainActivity.kt
+++ app/src/main/java/com/kapanen/hearthstoneassessment/MainActivity.kt
@@ -1,12 +1,8 @@
 package com.kapanen.hearthstoneassessment
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 import com.kapanen.hearthstoneassessment.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
