89e323ad90955896e1b3b6c1d9a8ab6cfa817298
Leonid Kapanen <leokapanen@gmail.com>
2022-05-25 14:53:53 +0300

    Use loading indicator and no data msg as part of card tabs

--- app/src/main/java/com/kapanen/hearthstoneassessment/ui/home/tab/CardsTabFragment.kt
+++ app/src/main/java/com/kapanen/hearthstoneassessment/ui/home/tab/CardsTabFragment.kt
@@ -4,15 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.kapanen.hearthstoneassessment.R
+import com.kapanen.hearthstoneassessment.databinding.FragmentCardsTabBinding
 import com.kapanen.hearthstoneassessment.delegate.AdapterDelegatesManager
 import com.kapanen.hearthstoneassessment.model.CardsTab
-import com.kapanen.hearthstoneassessment.model.LoadingItem
 import com.kapanen.hearthstoneassessment.model.NoDataItem
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -26,39 +24,68 @@ class CardsTabFragment : Fragment() {
     @Inject
     lateinit var adapterDelegatesManager: AdapterDelegatesManager
 
+    private var _binding: FragmentCardsTabBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_cards_tab, container, false)
+        _binding = FragmentCardsTabBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val cardsTabViewModel =
             ViewModelProvider(this)[CardsTabViewModel::class.java]
-        val recyclerView = view.findViewById<RecyclerView>(R.id.cards_recycler_view)
-
-        val cardsListAdapter = CardsListAdapter(adapterDelegatesManager)
-        recyclerView.adapter = cardsListAdapter
-        recyclerView.layoutManager = LinearLayoutManager(requireContext())
-        recyclerView.itemAnimator = null
-        cardsListAdapter.setItems(listOf(LoadingItem()))
-        (arguments?.get(ARGS_KEY) as CardsTab?)?.let { cardsTab ->
-            cardsTabViewModel.observeCards(cardsTab).observe(viewLifecycleOwner) { cards ->
-                if (cards.isNotEmpty()) {
-                    if (recyclerView.layoutManager !is GridLayoutManager) {
-                        recyclerView.layoutManager =
-                            GridLayoutManager(requireContext(), GRID_NUMBER_OF_COLUMNS)
+        binding.apply {
+            val cardsListAdapter = CardsListAdapter(adapterDelegatesManager)
+            cardsRecyclerView.adapter = cardsListAdapter
+            cardsRecyclerView.layoutManager =
+                GridLayoutManager(requireContext(), GRID_NUMBER_OF_COLUMNS)
+            cardsRecyclerView.itemAnimator = null
+            showLoading()
+            (arguments?.get(ARGS_KEY) as CardsTab?)?.let { cardsTab ->
+                cardsTabViewModel.observeCards(cardsTab).observe(viewLifecycleOwner) { cards ->
+                    if (cards.isNotEmpty()) {
+                        showCards()
+                        cardsListAdapter.setItems(cards)
+                    } else {
+                        showNoData()
+                        cardsListAdapter.setItems(listOf(NoDataItem()))
                     }
-                    cardsListAdapter.setItems(cards)
-                } else {
-                    if (recyclerView.layoutManager !is LinearLayoutManager) {
-                        recyclerView.layoutManager = LinearLayoutManager(requireContext())
-                    }
-                    cardsListAdapter.setItems(listOf(NoDataItem()))
                 }
             }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun showCards() {
+        binding.apply {
+            binding.cardsNoDataContainer.noDataContainer.isVisible = false
+            binding.cardsProgressContainer.progressContainer.isVisible = false
+            cardsRecyclerView.isVisible = true
+        }
+    }
+
+    private fun showLoading() {
+        binding.apply {
+            binding.cardsNoDataContainer.noDataContainer.isVisible = false
+            binding.cardsProgressContainer.progressContainer.isVisible = true
+            cardsRecyclerView.isVisible = false
+        }
+    }
+
+    private fun showNoData() {
+        binding.apply {
+            binding.cardsNoDataContainer.noDataContainer.isVisible = true
+            binding.cardsProgressContainer.progressContainer.isVisible = false
+            cardsRecyclerView.isVisible = false
         }
     }
 
