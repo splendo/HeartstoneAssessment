82fdf2414bb889def19d0e3479f4d6867225d78f
Leonid Kapanen <leokapanen@gmail.com>
2022-05-26 20:57:05 +0300

    Removing unused code.

--- app/src/main/java/com/kapanen/hearthstoneassessment/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/kapanen/hearthstoneassessment/ui/home/HomeViewModel.kt
@@ -5,15 +5,12 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.kapanen.hearthstoneassessment.R
-import com.kapanen.hearthstoneassessment.data.CardType
 import com.kapanen.hearthstoneassessment.data.CardsRepository
 import com.kapanen.hearthstoneassessment.di.AppModule
 import com.kapanen.hearthstoneassessment.model.CardsTab
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
@@ -23,11 +20,6 @@ class HomeViewModel @Inject constructor(
     private var cardTabs: List<CardsTab>,
     private val dispatcher: CoroutineDispatcher
 ) : ViewModel() {
-
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
-    }
-    val text: LiveData<String> = _text
 
     init {
         loadCards()
