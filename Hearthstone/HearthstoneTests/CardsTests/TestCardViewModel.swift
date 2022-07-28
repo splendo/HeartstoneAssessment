ce3e62bcd3479cfd149c9417a09e1d279e0a8679
Stavros Tsikinas <s.tsikinas@epsilonnet.gr>
2022-07-28 14:38:03 +0300

    add tests for view model

---
+++ Hearthstone/HearthstoneTests/CardsTests/TestCardViewModel.swift
@@ -0,0 +1,33 @@
+//
+//  TestCardViewModel.swift
+//  HearthstoneTests
+//
+//  Created by Epsilon User on 28/7/22.
+//
+
+import XCTest
+@testable import Hearthstone
+
+class TestCardViewModel: XCTestCase {
+
+    override func setUpWithError() throws {
+        super.setUp()
+        
+    }
+    
+    func testCardViewModelInitialization() {
+        let cardVM = CardViewModel(card: Card(name: "a Card", img: "https://www.cardlink.card/card.png"))
+        
+        XCTAssertNotNil(cardVM.title, "No title found on cardVM")
+        XCTAssertNotNil(cardVM.image, "No image path found on cardVM")
+    }
+    
+    func testCardViewModelTitlePlaceholder() {
+        
+        let cardVM = CardViewModel(card: Card())
+        
+        XCTAssertEqual(cardVM.title, CardViewModel.placeholderTitle, "Placeholder title should've been provided on an empty Card")
+        
+    }
+
+}
