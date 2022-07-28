38d3274c76c54fc4e0ba671ddb0be3eab7c79339
Stavros Tsikinas <s.tsikinas@epsilonnet.gr>
2022-07-28 11:00:30 +0300

    add tests for json file validation

---
+++ Hearthstone/HearthstoneTests/CardsTests/Helpers/CardsHelper.swift
@@ -0,0 +1,30 @@
+//
+//  CardsHelper.swift
+//  HearthstoneTests
+//
+//  Created by Stavros Tsikinas on 28/7/22.
+//
+
+import Foundation
+
+
+///
+/// Test helper method of retrieving the json url.
+///
+func getUrlFile(from name: String?, typed: String?) -> URL? {
+    
+    return Bundle.main.url(forResource: name, withExtension: typed)
+}
+
+///
+/// Test helper method of converting url to json string.
+///
+func getJSONString(from url: URL) throws -> String? {
+    
+    do {
+        return try String(contentsOf: url, encoding: .utf8)
+    } catch {
+        return nil
+    }
+    
+}
