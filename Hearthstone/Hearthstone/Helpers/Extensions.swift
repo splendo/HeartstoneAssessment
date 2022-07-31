5ff4406dd31a38d2d3d25d080fb398971b5415fb
Stavros Tsikinas <s.tsikinas@epsilonnet.gr>
2022-07-31 22:28:54 +0300

    setup grid item view (no view model added)

--- Hearthstone/Hearthstone/Helpers/Extensions.swift
+++ Hearthstone/Hearthstone/Helpers/Extensions.swift
@@ -16,29 +16,32 @@ extension UIImageView {
     // Also, seems cooler ;)
     static var placeholderImageUrl = URL(string: "https://via.placeholder.com/500x500.png?text=No+Image+Found")!
     
-    func load(from url: URL) {
+    func load(from url: URL, mode: UIView.ContentMode) {
         
-        DispatchQueue.global().async { [weak self] in
-            if let imgData = try? Data(contentsOf: url) {
-                if let img = UIImage(data: imgData) {
-                    DispatchQueue.main.async {
-                        self?.image = img
-                    }
-                } else {
-                    self?.addPlaceholderImage()
+        URLSession.shared.dataTask(with: url) {  [weak self] data, response, error in
+            guard
+                let httpURLResponse = response as? HTTPURLResponse,
+                    httpURLResponse.statusCode == 200,
+                let data = data, error == nil,
+                let image = UIImage(data: data)
+                else {
+                self?.addPlaceholderImage(with: mode)
+                    return
                 }
-            } else {
-                self?.addPlaceholderImage()
-            }
-        }
+                DispatchQueue.main.async() {
+                    self?.image = image
+                    self?.contentMode = mode
+                }
+            }.resume()
     }
     
-    private func addPlaceholderImage() {
+    private func addPlaceholderImage(with mode: UIView.ContentMode) {
         
         if let imgData = try? Data(contentsOf: UIImageView.placeholderImageUrl) {
             if let img = UIImage(data: imgData) {
                 DispatchQueue.main.async { [weak self] in
                     self?.image = img
+                    self?.contentMode = mode
                 }
             }
         }
@@ -58,6 +61,79 @@ extension UIColor {
     
 }
 
+extension UIView {
+    
+    /// Make view rounded (dashboard item)
+    func cardView(of radius: CGFloat, with shadow: UIColor? = nil) {
+        layer.cornerRadius = radius
+        if let shadow = shadow {
+            layer.shadowColor = shadow.cgColor
+            layer.shadowOffset = CGSize(width: 0.0, height: 0.0)
+            layer.shadowRadius = 12.0
+            layer.shadowOpacity = 0.7
+        }
+    }
+    
+    /// Add anchors to the view programmatically
+    func addAnchors(wAnchor: NSLayoutDimension? = nil, _ wMulti: CGFloat? = nil,
+                    hAnchor: NSLayoutDimension? = nil,_ hMulti: CGFloat? = nil,
+                    cXAnchor: NSLayoutXAxisAnchor? = nil,
+                    cYAnchor: NSLayoutYAxisAnchor? = nil,
+                    lAnchor: NSLayoutXAxisAnchor? = nil, leftConstant: CGFloat? = nil,
+                    tAnchor: NSLayoutYAxisAnchor? = nil, topConstant: CGFloat? = nil,
+                    rAnchor: NSLayoutXAxisAnchor? = nil, rightConstant: CGFloat? = nil,
+                    bAnchor: NSLayoutYAxisAnchor? = nil, bottomConstant: CGFloat? = nil,
+                    widthConstant: CGFloat? = nil,
+                    heightConstant: CGFloat? = nil) {
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        if let wAnchor = wAnchor {
+            widthAnchor.constraint(equalTo: wAnchor, multiplier: wMulti == nil ? 1.0 : wMulti!).isActive = true
+        }
+        
+        if let hAnchor = hAnchor {
+            heightAnchor.constraint(equalTo: hAnchor, multiplier: hMulti == nil ? 1.0 : hMulti!).isActive = true
+        }
+        
+        if let cXAnchor = cXAnchor {
+            centerXAnchor.constraint(equalTo: cXAnchor, constant: 0).isActive = true
+        }
+        
+        if let cYAnchor = cYAnchor {
+            centerYAnchor.constraint(equalTo: cYAnchor, constant: 0).isActive = true
+        }
+        
+        if let lAnchor = lAnchor {
+            leftAnchor.constraint(equalTo: lAnchor, constant: leftConstant == nil ? 0 : leftConstant!).isActive = true
+
+        }
+        
+        if let tAnchor = tAnchor {
+            topAnchor.constraint(equalTo: tAnchor, constant: topConstant == nil ? 0 : topConstant!).isActive = true
+
+        }
+        
+        if let rAnchor = rAnchor {
+            rightAnchor.constraint(equalTo: rAnchor, constant: rightConstant == nil ? 0 : rightConstant!).isActive = true
+        }
+        
+        if let bAnchor = bAnchor {
+            bottomAnchor.constraint(equalTo: bAnchor, constant: bottomConstant == nil ? 0 : bottomConstant!).isActive = true
+        }
+        
+        if let widthConstant = widthConstant {
+            widthAnchor.constraint(equalToConstant: widthConstant).isActive = true
+        }
+        
+        if let heightConstant = heightConstant {
+            widthAnchor.constraint(equalToConstant: heightConstant).isActive = true
+        }
+    }
+    
+}
+
 
 // - MARK: HomeTabViewController extensionss
 extension HomeTabViewController {
@@ -65,7 +141,6 @@ extension HomeTabViewController {
     func initView() {
         view.backgroundColor = .systemBackground
         tabBar.tintColor = .primaryColor
-        tabBar.backgroundColor = .accentColor
     }
     
 }
--- Hearthstone/Hearthstone/Views/ViewControllers/CardsCollectionViewController.swift
+++ Hearthstone/Hearthstone/Views/ViewControllers/CardsCollectionViewController.swift
@@ -2,16 +2,41 @@
 //  CardsCollectionViewController.swift
 //  Hearthstone
 //
-//  Created by Epsilon User on 29/7/22.
+//  Created by Stavros Tsikinas on 29/7/22.
 //
 
 import UIKit
 
-class CardsCollectionViewController: UICollectionViewController {
+class CardsCollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        collectionView.register(CardGridViewCell.self, forCellWithReuseIdentifier: "CardCell")
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 150, height: 250)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
     }
 
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath) as? CardGridViewCell else {
+            return UICollectionViewCell()
+        }
+        if let imageUrl = URL(
+            string: "https://wow.zamimg.com/images/hearthstone/cards/enus/original/FP1_014.png") {
+            cell.cardImage.load(from: imageUrl, mode: .scaleAspectFit)
+        }
+        
+        return cell
+    }
+    
 }
